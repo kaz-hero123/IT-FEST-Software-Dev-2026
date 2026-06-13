f897e2ba06482167babbf18fcd66a8963180f3b1
hilman-nidal-hamzi <hilmanhamzi@gmail.com>
2026-06-13 21:11:05 +0700

    "Feat: Complete all database migrations for Jelajah Madura"

---
+++ database/migrations/2026_06_13_135439_create_contents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('regency_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('address');
+            $table->string('maps_url')->nullable();
+            $table->enum('status',['pending','approved','rejected','deleted'])->default('pending');
+            $table->boolean('was_approved')->default(false);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
