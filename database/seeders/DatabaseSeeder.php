7374dd861c41d426770aca003e7b18ba65b395a5
hilman-nidal-hamzi <hilmanhamzi@gmail.com>
2026-07-26 22:58:18 +0700

    feat: add DemoContentSeeder with 12 items across Kuliner, UMKM, Spot Foto
    - 4 Kuliner: Bebek Sinjay, Soto Pak Nyak, Nasi Serpang, Kaldu Kokot
    
    - 4 UMKM: Ukir Kayu Arosbaya, Batik Tulis, Garam Rakyat, Keripik Singkong
    
    - 4 Spot Foto: Bukit Geger, Gua Lebar, Pantai Batu Kerbuy, Gili Labak
    
    - 4 demo contributors added for data variety
    
    - Total content: 24 items (12 wisata + 12 new) across all 4 regencies and categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             UserSeeder::class,
             WisataSeeder::class,
+            DemoContentSeeder::class,
             UserExploreDetailSeeder::class,
         ]);
     }
