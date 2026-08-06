c61474c6aff2d1289fe155474e46d7ce80141b0f
hilman-nidal-hamzi <hilmanhamzi@gmail.com>
2026-08-06 20:35:20 +0700

    hotfix: remove caching from landing to fix production 500

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -8,13 +8,11 @@ class LandingController extends Controller
 {
     public function index()
     {
-        $popularContents = \Illuminate\Support\Facades\Cache::remember('popular_contents', 300, function () {
-            return Content::with(['primaryPhoto', 'category', 'regency'])
-                ->where('status', 'approved')
-                ->orderByDesc('view_count')
-                ->limit(6)
-                ->get();
-        });
+        $popularContents = Content::with(['primaryPhoto', 'category', 'regency'])
+            ->where('status', 'approved')
+            ->orderByDesc('view_count')
+            ->limit(6)
+            ->get();
 
         return view('pages.user.home.user-home-index', compact('popularContents'));
     }
